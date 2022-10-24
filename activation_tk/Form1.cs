5155472cc6d538a8ec8b2cc643ed651d405fd7ee
thuutien <trantien.sm@gmail.com>
2022-10-24 14:53:34 -0500

    Add project files.

---
+++ activation_tk/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace activation_tk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bnt_10home_Click(object sender, EventArgs e)
+        {
+            Helper.activate(Windows.tenhome);
+        }
+
+        private void bnt_10pro_Click(object sender, EventArgs e)
+        {
+            Helper.activate(Windows.tenpro);
+        }
+
+        private void bnt_11home_Click(object sender, EventArgs e)
+        {
+            Helper.activate(Windows.elevenhome);
+        }
+
+        private void bnt_11pro_Click(object sender, EventArgs e)
+        {
+            Helper.activate(Windows.elevenpro);
+        }
+    }
+}
---
+++ activation_tk/Helper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace activation_tk
+{
+    internal class Helper
+    {
+        public static void activate(Windows version)
+        {
+            string key = "";
+
+            switch (version)
+            {
+                case Windows.tenhome:
+                    key = "slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99";
+                    break;
+                case Windows.tenpro:
+                    key = "slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX";
+                    break;
+                case Windows.elevenhome:
+                    key = "slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99";
+                    break;
+                case Windows.elevenpro:
+                    key = "slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX";
+                    break;
+            }
+            if (key != "")
+            {
+                Cursor.Current = Cursors.WaitCursor;
+                PowerShell ps = PowerShell.Create();
+                ps.AddScript(key);
+                ps.Invoke();
+                ps.AddScript("slmgr /skms kms8.msguides.com");
+                ps.Invoke();
+                ps.AddScript("slmgr /ato");
+                ps.Invoke();
+                Cursor.Current = Cursors.Default;
+                MessageBox.Show("Activation completed!");
+            }
+            else
+            {
+                MessageBox.Show("Error occurred, key is empty");
+            }
+            
+
+
+
+        }
+        
+    }
+
+    internal enum Windows
+    {
+        tenhome,
+        tenpro,
+        elevenhome,
+        elevenpro
+    }
+}
